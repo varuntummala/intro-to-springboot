a5da83eaba01bc5a343e29edac46160b1ccf273f
Varun Tummala <varunt@gmail.com>
2023-08-15 20:47:54 -0400

    Initial commit

---
+++ src/main/java/com/example/introtospringboot/IntroToSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.introtospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroToSpringbootApplication.class, args);
+    }
+
+}
